7631345410d23b76f6b04b716cefc2389857323d
pk <pipalpurus@gmail.com>
2021-06-12 12:16:04 +0545

    os module

---
+++ reference/os_demo.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+// Cpu Arc
+console.log(os.arch());
+
+// Cpu Core Info
+console.log(os.cpus());
+
+// Free Memory
+console.log(os.totalmem());
+
+// Total Memo
+console.log(os.freemem());
+
+// System up Time
+console.log(os.uptime());
